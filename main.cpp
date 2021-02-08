cd02bb85ead54e849c76a0a09a0ee4e586bbe0b3
user <kosyanip@gmail.com>
2021-02-08 18:41:11 +0300

    major update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 #define endl std::endl
-#define for_each std::for_each
 #define pair std::pair
 #define sort stable_sort
 #define string std::string
@@ -49,38 +48,40 @@ const int MOD=1e9+7;
 const int PRECISION=7;
 const double EPS = 1e-6;
 const double PI = std::acos(-1.0L);
-struct custom_tf:std::numpunct<char>{string do_truename()const{return"Yes";}string do_falsename()const{return"No";}};
-const auto boostIO=[](){std::ios_base::sync_with_stdio(0);std::cin.tie(0);return 0;}();
-const auto editIO=[](){std::cout<<std::fixed<<std::setprecision(PRECISION);std::cout<<std::boolalpha;std::cout.imbue(std::locale(std::cout.getloc(),new custom_tf));return 0;}();
 inline int mod(const int&a,const int&b=MOD){int r=a%b;return(r<0?r+b:r);}
-inline int pown(int n,int power,const int modulo=MOD){int res=1;while(power){if(power&1)res=mod(res*n,modulo);n=mod(n*n,modulo);power>>=1;}return res;}
+inline int pown(ll n,ll power,const ll modulo=MOD){int res=1;while(power){if(power&1)res=mod(res*n,modulo);n=mod(n*n,modulo);power>>=1;}return res;}
 
-void read(std::istream&cin=std::cin) {
+std::istream&in=std::cin;
+std::ostream&out=std::cout;
+const auto boostIO=[](){std::ios_base::sync_with_stdio(0);in.tie(0);return 0;}();
+const auto editIO=[](){out<<std::fixed<<std::setprecision(PRECISION);return 0;}();
+
+void read() {
   
 }
 
-void solve() {
+auto solve() {
   
 }
 
-void write(std::ostream&cout=std::cout) {
+void write() {
   
+  #ifdef LOCAL_PROJECT
+    out.flush();
+  #endif
 }
 
 int32_t main()
 {
-  int t;std::cin>>t;
+  int t;in>>t;
   fq(t) {
-    
     read();
     solve();
     write();
   }
 
-  std::cout.flush();
+  out.flush();
   getchar();
   return 0;
 }
-
-
 
