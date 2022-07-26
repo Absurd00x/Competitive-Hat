f9ce8f44efcf0d591c18fef154d76fd67eeba044
user <kosyanip@gmail.com>
2022-07-26 17:39:49 +0300

    added geometry <3

---
+++ Algorithms/Advanced/Geom.cpp
@@ -0,0 +1,21 @@
+typedef tuple<int, int, int> ti;
+typedef pair<int, int> pii;
+
+ti get_line(const pii &a, const pii &b) {
+  assert(a != b);
+  ti res;
+  auto &[A, B, C] = res;
+  auto &[x1, y1] = a;
+  auto &[x2, y2] = b;
+  A = y1 - y2;
+  B = x2 - x1;
+  C = x1 * y2 - x2 * y1;
+  int g = gcd(A, gcd(B, C));
+  A /= g;
+  B /= g;
+  C /= g;
+  if (A < 0 || (A == 0 && B < 0)) {
+    A = -A, B = -B, C = -C;
+  }
+  return res;
+}
