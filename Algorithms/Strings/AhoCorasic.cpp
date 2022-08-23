006703ec8697adf1c37a49042e892989c1548021
desktop <kosyanip@gmail.com>
2022-08-23 12:50:07 +0300

    returned Aho-Corasic

--- Algorithms/Strings/AhoCorasic.cpp
+++ Algorithms/Strings/AhoCorasic.cpp
@@ -1,71 +1,56 @@
 const int K = 'z' - 'a' + 1;
-typedef unsigned char uc;
+const int NONE{-1};
 
 struct Node {
-  int next[K];
-  bool is_term;
-  int prev;
+  int32_t to[K];
   char psymb;
-  int link;
-  Node() : is_term(0), prev(0), psymb(0), link(0) {
-    memset(next, 0, sizeof(next));
+  int32_t prev, link, id;
+  Node(): psymb('\0'), prev(0), link(0), id(NONE) {
+    memset(to, 0, sizeof(to));
   }
 };
 
 class AhoCorasic {
 private:
-
-  uc get_ind(char c) {
-    return uc(c - 'a');
-  }
-
+  vector<int32_t> table;
 public:
   vector<Node> guts;
 
-  void build(const vector<string> &words, int MBsize) {
-    int Bsize = MBsize * 1024 * 1024;
-    guts.resize(Bsize / sizeof(Node));
+  AhoCorasic(const int MAX_NODES=1'000'000): table(256, NONE), guts(MAX_NODES) {}
+
+  void build(vector<string> &words, const string &symbs) {
+    for (int32_t i = 0; i < (int32_t)symbs.size(); ++i) {
+      table[symbs[i]] = i;
+    }
     guts.clear();
     guts.push_back(Node());
-    // добавляю строчки в бор
-    for (auto &s : words) {
-      int v = 0;
-      for (char c : s) {
-        uc i = get_ind(c);
-        if (guts[v].next[i] == 0) {
-          guts[v].next[i] = (int)guts.size();
+    for (int32_t i = 0; i < (int32_t)words.size(); ++i) {
+      int32_t v = 0;
+      for (char c : words[i]) {
+        int32_t ind = table[c];
+        if (guts[v].to[ind] == 0) {
+          guts[v].to[ind] = (int32_t)guts.size();
           guts.push_back(Node());
-          guts.back().prev = v;
           guts.back().psymb = c;
+          guts.back().prev = v;
         }
-        v = guts[v].next[i];
+        v = guts[v].to[ind];
       }
-      guts[v].is_term = true;
+      guts[v].id = i;
     }
-    queue<int> q;
-    // у корня и его детей суффиксная ссылка всегда ведёт в корень
-    for (int child : guts[0].next) {
-      if (child != 0) {
-        for (int i = 0; i < K; ++i) {
-          int &next = guts[v].next[i];
-          if (next != 0) {
-            q.push(next);
-          } else {
-            next = guts[0].to[i];
-          }
-        }
-      }
-    }
+    queue<int32_t> q;
+    q.push(0);
     while (!q.empty()) {
-      int v = q.front();
+      int32_t cur = q.front();
       q.pop();
-      int prev_link = guts[guts[v].prev].link;
-      int cur_link = guts[prev_link].next[get_ind(guts[v].psymb)];
-      guts[v].link = cur_link;
-      for (uc i = 0; i < K; ++i) {
-        int &next = guts[v].next[i];
+      if (guts[cur].prev != 0) {
+        int32_t prev_link = guts[guts[cur].prev].link;
+        guts[cur].link = guts[prev_link].to[table[guts[cur].psymb]];
+      }
+      for (int i = 0; i < K; ++i) {
+        int32_t &next = guts[cur].to[i];
         if (next == 0) {
-          next = guts[cur_link].next[i];
+          next = guts[guts[cur].link].to[i];
         } else {
           q.push(next);
         }
@@ -73,7 +58,65 @@ public:
     }
   }
 
-  int step(int v, char c) {
-    return guts[v].next[get_ind(c)];
+  int go(int v, char c) {
+    return guts[v].to[table[c]];
+  }
+} ac;
+
+// OOM
+
+const int K = 'z' - 'a' + 1;
+const int NONE{-1};
+
+struct Node {
+  map<char, int32_t> to;
+  char psymb{'\0'};
+  int32_t prev{NONE};
+  int32_t link{NONE};
+  int32_t id{NONE};
+};
+
+class AhoCorasic {
+public:
+  vector<Node> guts;
+
+  AhoCorasic(const int MAX_NODES=1'000'000) {
+    guts.resize(MAX_NODES);
+  }
+
+  void build(vector<string> &words) {
+    guts.clear();
+    guts.push_back(Node());
+    for (int32_t i = 0; i < (int32_t)words.size(); ++i) {
+      int32_t v = 0;
+      for (char c : words[i]) {
+        if (guts[v].to.count(c) == 0) {
+          guts[v].to[c] = (int32_t)guts.size();
+          guts.push_back(Node());
+          guts.back().psymb = c;
+          guts.back().prev = v;
+        }
+        v = guts[v].to[c];
+      }
+      guts[v].id = i;
+    }
+  }
+  int get_link(int v) {
+    if (guts[v].link != NONE) {
+      return guts[v].link;
+    } else if (guts[v].prev != 0){
+      return guts[v].link = (int32_t)go(get_link(guts[v].prev), guts[v].psymb);
+    } else {
+      return guts[v].link = 0;
+    }
+  }
+  int go(int v, char c) {
+    if (guts[v].to.count(c)) {
+      return guts[v].to[c];
+    } else if (v != 0) {
+      return guts[v].to[c] = (int32_t)go(get_link(v), c);
+    } else {
+      return guts[v].to[c] = 0;
+    }
   }
 } ac;
