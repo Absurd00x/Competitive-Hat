579b83bf62af5d1f22b2b9c2ca131361a0b07df9
desktop <kosyanip@gmail.com>
2022-12-28 16:05:50 +0300

    only ll in get_line

--- Algorithms/Geom/Get_line.cpp
+++ Algorithms/Geom/Get_line.cpp
@@ -1,5 +1,5 @@
-typedef tuple<int, int, ll> ti;
-typedef pair<int, int> pii;
+typedef tuple<ll, ll, ll> ti;
+typedef pair<ll, ll> pii;
 
 ti get_line(const pii &a, const pii &b) {
   assert(a != b);
@@ -11,8 +11,8 @@ ti get_line(const pii &a, const pii &b) {
   B = x2 - x1;
   C = x1 * y2 - x2 * y1;
   ll g = gcd(A, gcd(B, C));
-  A = int(A / g);
-  B = int(A / g);
+  A /= g;
+  B /= g;
   C /= g;
   if (A < 0 || (A == 0 && B < 0)) {
     A = -A, B = -B, C = -C;
