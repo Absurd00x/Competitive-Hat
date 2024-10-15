955e6d5b3671c472263fbdce72b67eeb81715f4d
Kirill <49096802+Absurd00x@users.noreply.github.com>
2024-10-15 13:31:12 +0300

    Update main.cpp
    4 spaces

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #ifdef LOCAL_PROJECT
-  #pragma GCC optimize("O0")
+    #pragma GCC optimize("O0")
 #else
-  #pragma GCC optimize("O3")
+    #pragma GCC optimize("O3")
 #endif
 #include <bits/stdc++.h>
 
@@ -16,22 +16,22 @@ typedef long double ld;
 #define rall(x) std::rbegin(x),std::rend(x)
 
 const auto boostIO = []() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout << fixed;
-  cout.precision(12);
-  return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout << fixed;
+    cout.precision(12);
+    return 0;
 }();
 
 
 
 signed main() {
-  int tests;
-  while (tests --> 0) {
-    read();
-    solve();
-    write();
-  }
-  cout.flush();
-  return 0;
+    int tests;
+    while (tests --> 0) {
+        read();
+        solve();
+        write();
+    }
+    cout.flush();
+    return 0;
 }
